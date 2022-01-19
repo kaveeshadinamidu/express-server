a5558687934b5a3a947a3f3724f375327dec7512
Kaveesha Dinamidu <kaveesha.19@cse.mrt.ac.lk>
2022-01-19 19:05:52 +0530

    Code refined

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const express = require('express')
-
+const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
+const router = express.Router();
+
+app.set('view-engine', 'ejs')
+app.use(express.static('public'));
+router.use(bodyParser.json());
+router.use(bodyParser.urlencoded({extended : true}));
 
 app.get('/',(req,res)=>{
     res.send('Hello World!');
